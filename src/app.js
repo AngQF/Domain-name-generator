8a4291696714e2ccdbcbdd4216922b8cdeb30592
AngQF <angela.qf@hotmail.com>
2022-03-17 21:44:54 +0000

    cambio

--- src/app.js
+++ src/app.js
@@ -21,7 +21,7 @@ for (let i = 0; i < pronoun.length; i++) {
   for (let j = 0; j < adj.length; j++) {
     for (let k = 0; k < noun.length; k++) {
       for (let l = 0; l < extensions.length; l++) {
-        dominios += pronoun[i] + adj[j] + noun[k] + extensions[l] + " ";
+        dominios += pronoun[i] + adj[j] + noun[k] + extensions[l] + ", ";
         console.log(dominios);
       }
     }
